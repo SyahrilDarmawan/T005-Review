5a0e10a7682a057a3ea484551915ffa805536a21
Muhammad Syahril Darmawan <123642882+SyahrilDarmawan@users.noreply.github.com>
2025-02-28 18:21:42 +0700

    Belum Lengkap

---
+++ academic/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name":"Academic Information System v1.0",
+    "version":"1.0",
+    "depends": ["base","board"],
+    "author":"your name",
+    "category":"Education",
+    "website":"your.website.com",
+    "description":"""long description
+about your moduels
+""",
+    "data":[
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "course.xml",
+        "session.xml",
+        "attendee.xml",
+        "partner.xml",
+        "dashboard.xml",
+    ],
+    "installable":True,
+    "auto_install":False,
+    "application":True,
+}
---
+++ academic/session.py
@@ -0,0 +1,66 @@
+from odoo import api,fields,models,_
+from odoo.exceptions import ValidationError
+import time
+
+class session(models.Model):
+    _name = "academic.session"
+
+    name = fields.Char(string="Name", required=True)
+
+    course_id = fields.Many2one(comodel_name="academic.course",
+                       string="Course", required=True)
+    
+    instructor_id = fields.Many2one(comodel_name="res.partner",
+                       string="Instructor", required=True)
+    
+    start_date = fields.Date(string="Start Date", default=lambda self: time.strftime("%Y-%m-%d") )
+    duration = fields.Integer(string="Duration")
+    seats = fields.Integer(string="Seats")
+    active = fields.Boolean(string="Active", default=True)
+
+    attendee_ids = fields.One2many(
+        comodel_name ="academic.attendee",
+        inverse_name = "session_id",
+        string="Attendees"
+    )
+
+    taken_seats = fields.Float(string="Taken Seats",
+                               compute="_calc_taken_seats")
+    
+    image_small = fields.Binary(string="Image Small", )
+
+
+    def _calc_taken_seats(self):
+        for rec in self:
+            if rec.seats > 0:
+                rec.taken_seats = 100.0 * len(rec.attendee_ids) / rec.seats
+            else:
+                rec.taken_seats = 0.0
+
+
+            # Penggunaan api ada disini
+
+    @api.onchange('seats','attendee_ids')
+    def onchange_seats(self):
+        for rec in self:
+            if rec.seats > 0:
+                rec.taken_seats = 100.0 * len(rec.attendee_ids) / rec.seats
+            else:
+                rec.taken_seats = 0.0
+        
+    
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _cek_instructor(self):
+        for session in self:
+
+            # partner_ids = []
+            # for att in session.attendee_ids:
+            #     partner_ids.append(att.partner_id.id)
+                        # Pake yang atas boleh, yang bawah juga boleh, sama aja.
+            partner_ids = [ att.partner_id.id for att in session.attendee_ids ]
+
+
+            # Jika datanya tidak valid alias data diatas sudah terpenuhi maka akan menjadi
+            if session.instructor_id.id in partner_ids:
+                raise ValidationError('Instructor tidak boleh menjadi bagian dari Peserta!')
+            
